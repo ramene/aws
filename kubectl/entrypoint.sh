5801265fb29f75e5c253b97d9b1f3e611cd231f5
Ramene Anthony <anthonyramene@gmail.com>
2019-09-18 19:22:29 -0400

    adaptation on kubectl container

--- kubectl/entrypoint.sh
+++ kubectl/entrypoint.sh
@@ -3,8 +3,10 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -exou pipefail
 
+env 
+
 # If the GITHUB_ACTION variable is set, we'll do some extra things to make common tasks easier.
-if [ ! -z "$GITHUB_ACTION" ]; then
+if [ ! -z "${KUBE_CONFIG_DATA:-}" ]; then
 
     env
 
@@ -17,9 +19,11 @@ fi
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 # TODO: Pass ARGS from Dockerfile for specific version(s) of aws-auth if so inclined
 # IDEA: Allow using `jq` to query `pulumi stack export`, similar to storing terraform statefile in s3 - or maybe push our stack outputs back to GitHub and query specific, tagged releases which we would need to store anyway... I think
-curl -O https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/aws-auth-cm.yaml
-export EKS_WORKER_ROLE=$( $pulumi_ci/stack | jq -r '.Stacks[0].Outputs[]|select(.OutputKey=="NodeInstanceRole")|.OutputValue')
-sed -i -e "s#<ARN of instance role (not instance profile)>#${EKS_WORKER_ROLE}#g" aws-auth-cm.yaml
+
+ls -alF
+echo "\`curl -O https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/aws-auth-cm.yaml\`"
+# export EKS_WORKER_ROLE=$( $pulumi_ci/stack | jq -r '.Stacks[0].Outputs[]|select(.OutputKey=="NodeInstanceRole")|.OutputValue')
+# sed -i -e "s#<ARN of instance role (not instance profile)>#${EKS_WORKER_ROLE}#g" aws-auth-cm.yaml
 
 # TODO: `base64 --decode` here
 echo "\`cat ${pulumi_ci}/kubeconfig\`"
